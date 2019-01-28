cdfb479f8df727d75278207fe47bc0cf98cc22e1
Adam Ahrens <adam.aahrens@gmail.com>
2019-01-28 12:12:51 -0600

    Fixing tests

--- spec/policies/user_spec.rb
+++ spec/policies/user_spec.rb
@@ -1,21 +1,18 @@
 require 'rails_helper'
 require 'support/factory_bot'
 
-RSpec.describe 'users can delete posts' do
-  context 'when viewing their own post' do
-    it 'should be able to delete the post' do
-      user = FactoryBot.create(:user)
-      post = Post.create(content: 'My first post', user: user)
-      expect(user.posts.count).to eq(1)
-    end
+RSpec.describe PostPolicy do
+  subject { described_class.new(user, post) }
+
+  context 'being the owner of a post' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:post) { Post.create(content: 'My first post', user: user) }
+    it { is_expected.to permit_action(:destroy) }
   end
 
-  context 'when viewing another post' do
-    it 'should not be able to delete the post' do
-      user = FactoryBot.create(:user)
-      other = FactoryBot.create(:other)
-      post = Post.create(content: 'My first post', user: user)
-      expect(user.posts.count).to eq(1)
-    end
+  context 'not being the owner of a post' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:post) { Post.create(content: 'My first post', user: FactoryBot.create(:other)) }
+    it { is_expected.to forbid_action(:destroy) }
   end
 end
